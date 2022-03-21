e8b26c6fa8eba02d342bd69746275578e36164ee
elian2310 <elianpacheco23.10@gmail.com>
2022-03-21 18:37:12 -0400

    Inicial

---
+++ PFPatrones/PFPatrones/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PFPatrones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
